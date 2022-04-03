c62407d91f22403d4c47a94967a92d6b70943a5a
Abduliitr <abdul.thomso@gmail.com>
2022-04-03 14:51:20 +0530

    Day 1 - Introduction to Python

---
+++ day1/explanation.py
@@ -0,0 +1,43 @@
+## Python variables
+
+# Definition
+
+a = 10
+b = 0.005
+name = "coders pavilion"
+
+# Memory Allocation
+
+'''
+
+Integers/Float variables usually takes 8 bytes of memory (in most languages).
+
+8 bytes = 8 * 4 bits = 32 bits.
+
+'''
+
+## Python data types - Integer, Float, String, Tuple
+
+intVariable = 100
+floatVariable = 10.12
+stringVariable = "Whats your name ?"
+tupleVariable = (10, 20)
+
+## Python input / output
+
+inputName = input()
+print("Hellooo" , inputName)
+
+## Python comments
+
+# Single line comment
+
+"""
+Multi
+Line
+Comment
+"""
+
+## Python errors
+
+print(unknownVariable)      ## NameError: name 'unknownVariable' is not defined
